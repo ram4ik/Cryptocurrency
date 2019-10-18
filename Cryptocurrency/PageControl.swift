d3b23e9cda97cdf7ed6b30fae13f215be03670e2
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-18 16:42:58 +0300

    class Coordinator

--- Cryptocurrency/PageControl.swift
+++ Cryptocurrency/PageControl.swift
@@ -14,16 +14,36 @@ struct PageControl: UIViewRepresentable {
     
     @Binding var currentPage: Int
     
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
     func makeUIView(context: Context) -> UIPageControl {
         let control = UIPageControl()
         control.numberOfPages = numberOfPages
         control.pageIndicatorTintColor = UIColor.systemGray
         control.currentPageIndicatorTintColor = UIColor(red: 85/255, green: 202/255, blue: 203/255, alpha: 1)
         control.frame(forAlignmentRect: CGRect(origin: .zero, size: CGSize(width: 30, height: 30)))
+        
+        control.addTarget(context.coordinator, action: #selector(Coordinator.updateCurrentPage(sender:)), for: .valueChanged)
+        
         return control
     }
     
     func updateUIView(_ uiView: UIPageControl, context: Context) {
         uiView.currentPage = currentPage
+    }
+    
+    class Coordinator: NSObject {
+        
+        var control: PageControl
+        
+        init(_ control: PageControl) {
+            self.control = control
+        }
+        
+        @objc func updateCurrentPage(sender: UIPageControl) {
+            control.currentPage = sender.currentPage
+        }
     }
 }
