07b6e29f97bcf13d3ac6844d00f53027ee1662d5
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-18 16:22:52 +0300

    pageViewController

--- Cryptocurrency/PageView.swift
+++ Cryptocurrency/PageView.swift
@@ -12,12 +12,17 @@ struct PageView<Page: View>: View {
     
     var viewControllers: [UIHostingController<Page>]
     
+    @State var currentPage: Int = 0
+    
     init(_ views: [Page]) {
         self.viewControllers = views.map { UIHostingController(rootView: $0)}
     }
     
     var body: some View {
-        PageViewController(controllers: viewControllers)
+        VStack {
+            PageViewController(controllers: viewControllers, currentPage: $currentPage)
+            Text("\(currentPage)")
+        }
     }
 }
 
--- Cryptocurrency/PageViewController.swift
+++ Cryptocurrency/PageViewController.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct PageViewController: UIViewControllerRepresentable {
     var controllers: [UIViewController]
     
+    @Binding var currentPage: Int
+    
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
@@ -24,10 +26,10 @@ struct PageViewController: UIViewControllerRepresentable {
     }
     
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
-        pageViewController.setViewControllers([controllers[0]], direction: .forward, animated: true)
+        pageViewController.setViewControllers([controllers[currentPage]], direction: .forward, animated: true)
     }
     
-    class Coordinator: NSObject, UIPageViewControllerDataSource {
+    class Coordinator: NSObject, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
         var parent: PageViewController
         
         init(_ pageViewController: PageViewController) {
@@ -56,6 +58,16 @@ struct PageViewController: UIViewControllerRepresentable {
             }
             
             return parent.controllers[index + 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+            
+            if completed,
+                let visibleViewController = pageViewController.viewControllers?.first,
+                let index = parent.controllers.firstIndex(of: visibleViewController) {
+                
+                parent.currentPage = index
+            }
         }
     }
 }
